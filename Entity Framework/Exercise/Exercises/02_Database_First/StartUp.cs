918bce74a27d994eb87e8ab7a77f855c057f8bb0
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-02-26 22:58:11 +0200

    Added exercise.

---
+++ Entity Framework/Exercise/Exercises/02_Database_First/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace SoftUni
+{
+    using System;
+    using SoftUni.Data;
+    using SoftUni.Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // DB Scaffold powershell cmdlet:
+            // Scaffold-DbContext -Connection "Server=DESKTOP-R3F6I64\SQLEXPRESS;Database=SoftUni;Integrated Security=True;" -Provider Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data/Models
+
+        }
+    }
+}
