f10dcc0cd96407b5861ded2c909812c32b8e7d8b
SonnyRR <9307969+SonnyRR@users.noreply.github.com>
2019-03-06 17:41:40 +0200

    * PatientMedicaments.cs: Added mapping table for patient's medicaments.
    * Doctor.cs: Added new entity.
    
    * VisitationConfig.cs: Added config for Visitation entity model.
    
    * PatientConfig.cs: Added config for Patient entity model.

---
+++ Code First/Exercise/01. HospitalDatabase/Data/ModelConfigs/PatientConfig.cs
@@ -0,0 +1,36 @@
+﻿namespace P01_HospitalDatabase.Data.ModelConfigs
+{
+    using Microsoft.EntityFrameworkCore;
+    using Data.Models;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class PatientConfig : IEntityTypeConfiguration<Patient>
+    {
+
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.HasKey(x => x.PatientId);
+
+            builder.Property(x => x.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(true)
+                .IsRequired(true);
+
+            builder.Property(x => x.LastName)
+            .HasMaxLength(50)
+            .IsUnicode(true)
+            .IsRequired(true);
+
+            builder.Property(x => x.Address)
+            .HasMaxLength(250)
+            .IsUnicode(true)
+            .IsRequired(true);
+
+            builder.Property(x => x.Email)
+            .HasMaxLength(80)
+            .IsUnicode(false)
+            .IsRequired(true)
+
+        }
+    }
+}
