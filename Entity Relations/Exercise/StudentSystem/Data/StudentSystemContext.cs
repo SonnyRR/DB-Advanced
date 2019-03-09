4c4ab107676efdc8795928d351e2f07f76f3682e
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-03-09 13:52:14 +0200

    Added exercise solution.

---
+++ Entity Relations/Exercise/StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,40 @@
+﻿namespace P01_StudentSystem.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Data.Models;
+    using Data.EntityConfigs;
+
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+           
+        }
+
+        public StudentSystemContext(DbContextOptions options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-R3F6I64\SQLEXPRESS;Database=StudentSystem;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new StudentCourseConfig());
+        }
+    }
+}
