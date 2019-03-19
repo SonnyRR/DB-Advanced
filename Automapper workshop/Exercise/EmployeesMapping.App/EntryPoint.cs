d618d03d6ee84b1b4c6a996627fa01b375d4c486
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-03-19 15:17:52 +0200

    Added solution.

---
+++ Automapper workshop/Exercise/EmployeesMapping.App/EntryPoint.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeesMapping.App
+{
+    using System;
+    using System.Linq;
+    using CustomMapper;
+    using EmployeesMapping.App.DTO;
+    using EmployeesMapping.Data;
+
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            using (EmployeesMappingContext db = new EmployeesMappingContext())
+            {
+                var employees = db.Employees
+                    .ToList();
+
+                var mapper = new Mapper();
+
+                var mapped = employees
+                    .Select(x => mapper.Map<EmployeeDto>(x))
+                    .ToList();
+            }
+        }
+    }
+}
---
+++ Automapper workshop/Exercise/EmployeesMapping.Data/Migrations/20190319123908_SecondMigration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeesMapping.Data.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Address", "BirthDay", "FirstName", "LastName", "Salary" },
+                values: new object[,]
+                {
+                    { 1, null, null, "Georgi", "Georgiev", 12131.44m },
+                    { 2, null, null, "Maria", "Marieva", 999.10m },
+                    { 3, null, null, "Alisia", "Alisieva", 11111.11m },
+                    { 4, null, null, "Pesho", "Peshov", 431.44m },
+                    { 5, null, null, "Vyara", "Marinova", 2000.44m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
