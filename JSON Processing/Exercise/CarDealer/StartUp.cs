8ee91570f2f274e32c5cf4b2a190d907d6697cdc
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-03-27 19:11:42 +0200

    Added new solution.

---
+++ JSON Processing/Exercise/CarDealer/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace CarDealer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.IO;
+    using System.Linq;
+
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.Models;
+    using Newtonsoft.Json;
+
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            
+        }
+
+        private static bool IsValid(object @object)
+        {
+            ICollection<ValidationResult> validations = new List<ValidationResult>();
+
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(@object);
+
+            bool isValid = Validator.TryValidateObject(@object, validationContext, validations, true);
+
+            return isValid;
+        }  
+
+        public static void Insert()
+        {
+            string carsPath = @"../../../Datasets/cars.json";
+            string customersPath = @"../../../Datasets/customers.json";
+            string partsPath = @"../../../Datasets/parts.json";
+            string salesPath = @"../../../Datasets/sales.json";
+            string suppliersPath = @"../../../Datasets/suppliers.json";
+
+            if (File.Exists(""))
+            {
+                var ImportData = File.ReadAllText();
+
+                using (var context = new CarDealerContext())
+                {
+                    string output;
+                    Console.WriteLine(output);
+                }
+
+            }
+        }
+}
