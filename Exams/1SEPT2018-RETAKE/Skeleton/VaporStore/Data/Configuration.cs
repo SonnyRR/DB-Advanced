7e7336ee8c5c5a5a01f6e4b29a50ed12be0f4946
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-04-02 15:23:42 +0300

    Added skeleton and model definitions.

--- Exams/1SEPT2018-RETAKE/Skeleton/VaporStore/Data/Configuration.cs
+++ Exams/1SEPT2018-RETAKE/Skeleton/VaporStore/Data/Configuration.cs
@@ -3,6 +3,6 @@
 	public static class Configuration
 	{
 		public static string ConnectionString =
-			@"Server=.;Database=VaporStore;Trusted_Connection=True";
+            @"Server=DESKTOP-R3F6I64\SQLEXPRESS;Database=VaporStore;Trusted_Connection=True";
 	}
 }
---
+++ Exams/1SEPT2018-RETAKE/Skeleton/VaporStore/Data/Models/Card.cs
@@ -0,0 +1,25 @@
+﻿namespace VaporStore.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using VaporStore.Data.Enums;
+
+    public class Card
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [RegularExpression(@"^(?<groups>[\d]{4} [\d]{4} [\d]{4} [\d]{4})$")]
+        public string Number { get; set; }
+
+        [RegularExpression(@"^[\d]{3}$")]
+        public string Cvc { get; set; }
+
+        public CardType Type { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Purchase> Purchases  { get; set; }
+    }
+}
