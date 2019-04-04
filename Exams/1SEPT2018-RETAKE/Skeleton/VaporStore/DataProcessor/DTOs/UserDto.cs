d408374f371bd6c14288b02b0e6a075e5976356d
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-04-04 14:20:11 +0300

    Fixed missing attribute for FullName.

--- Exams/1SEPT2018-RETAKE/Skeleton/VaporStore/DataProcessor/DTOs/UserDto.cs
+++ Exams/1SEPT2018-RETAKE/Skeleton/VaporStore/DataProcessor/DTOs/UserDto.cs
@@ -8,6 +8,7 @@
         public string Username { get; set; }
 
         [RegularExpression(@"^(?<name>[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+)$")]
+        [Required]
         public string FullName { get; set; }
 
         [Required]
