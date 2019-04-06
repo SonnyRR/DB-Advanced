ada7595a0cef0d958246b0fd671ce0db17b211c1
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-04-06 11:36:48 +0300

    Added helper class.

---
+++ Exams/12AUG2018/Skeleton/SoftJail/DataProcessor/KotsevExamHelper.cs
@@ -0,0 +1,88 @@
+﻿namespace SoftJail.DataProcessor
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public static class KotsevExamHelper
+    {
+        /// <summary>
+        /// Gets an object instance from DbSet T
+        /// </summary>
+        /// <returns>The object from set.</returns>
+        /// <param name="predicate">Predicate.</param>
+        /// <param name="context">Context.</param>
+        /// <typeparam name="T">Type of object from set</typeparam>
+        /// <typeparam name="V">Type of DbContext</typeparam>
+        private static T GetObjectFromSet<T, V>(Func<T, bool> predicate, V context)
+            where T : class, new()
+            where V : DbContext
+        {
+            var dbSetType = context.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .FirstOrDefault(p => p.PropertyType.IsAssignableFrom(typeof(DbSet<T>)));
+
+            if (dbSetType == null)
+                throw new ArgumentException($"DbSet with type: {dbSetType.Name} does not exist!");
+
+            DbSet<T> set = (DbSet<T>)dbSetType
+                .GetMethod
+                .Invoke(context, null);
+
+            T desiredObject = set
+                .FirstOrDefault(predicate);
+
+            return desiredObject;
+        }
+
+        public static string SerializeObject<T>(T values, string rootName, bool omitXmlDeclaration = false,
+            bool indentXml = true)
+        {
+            string xml = string.Empty;
+
+            var serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(rootName));
+
+            var settings = new XmlWriterSettings()
+            {
+                Indent = indentXml,
+                OmitXmlDeclaration = omitXmlDeclaration
+            };
+
+            XmlSerializerNamespaces @namespace = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+
+            using (var stream = new StringWriter())
+            using (var writer = XmlWriter.Create(stream, settings))
+            {
+                serializer.Serialize(writer, values, @namespace);
+                xml = stream.ToString();
+            }
+
+            return xml;
+        }
+
+        [Obsolete]
+        private static bool IsEntityEqual
+            (this PropertyInfo[] properties, string[] propertyNames, object[] values, object obj)
+        {
+            bool isEqual = true;
+
+            properties = properties.Where(p => propertyNames.Contains(p.Name)).ToArray();
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                if ((string)properties[i].GetValue(obj) != (string)values[i])
+                {
+                    isEqual = false;
+                    break;
+                }
+            }
+
+            return isEqual;
+        }
+    }
+}
