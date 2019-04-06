78ae8a10237c74a1888cf35b4a5c26216c085ce9
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-04-06 11:25:45 +0300

    Added exam and model definition structure.

---
+++ Exams/12AUG2018/Skeleton/SoftJail/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftJail.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-R3F6I64\SQLEXPRESS;Database=SoftJail;Trusted_Connection=True";
+    }
+}
